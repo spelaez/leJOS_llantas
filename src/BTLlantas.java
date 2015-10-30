e6256b45e567ffca58ece14de8ad17494350ca35
Santiago <santi_rua@hotmail.com>
2015-10-29 22:29:11 -0500

    Movi de paquete la clase BTLlantas

---
+++ src/BTLlantas.java
@@ -0,0 +1,53 @@
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import lejos.nxt.LCD;
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.Bluetooth;
+
+public class BTLlantas {
+
+	public static void main(String [] args)  throws Exception 
+	{
+		String connected = "Connected";
+        String waiting = "Waiting...";
+        String closing = "Closing...";
+        
+		while (true)
+		{
+			LCD.drawString(waiting,0,0);
+			LCD.refresh();
+
+	        BTConnection btc = Bluetooth.waitForConnection();
+	        
+			LCD.clear();
+			LCD.drawString(connected,0,0);
+			LCD.refresh();	
+
+			DataInputStream dis = btc.openDataInputStream();
+			DataOutputStream dos = btc.openDataOutputStream();
+			
+			for(int i=0;i<100;i++) {
+				int n = dis.readInt();
+				LCD.drawInt(n, 5, 5);
+			}
+			Thread.sleep(2000);
+			dis.close();
+			Thread.sleep(2000);
+			dos.close();
+			Thread.sleep(2000); // wait for data to drain
+			LCD.clear();
+			LCD.drawString(closing,0,0);
+			LCD.refresh();
+			Thread.sleep(2000);
+			btc.close();
+			LCD.clear();
+			
+		   
+		}
+		
+	}
+}
