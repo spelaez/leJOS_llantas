ac5f04a41f9b23322981e23a86eede511cb100fa
Santiago <santi_rua@hotmail.com>
2015-10-29 22:42:58 -0500

    Modifique archivo BTLlantas

--- src/BTLlantas.java
+++ src/BTLlantas.java
@@ -1,5 +1,4 @@
 
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -28,21 +27,18 @@ public class BTLlantas {
 			LCD.refresh();	
 
 			DataInputStream dis = btc.openDataInputStream();
-			DataOutputStream dos = btc.openDataOutputStream();
 			
 			for(int i=0;i<100;i++) {
 				int n = dis.readInt();
 				LCD.drawInt(n, 5, 5);
+				
 			}
 			Thread.sleep(2000);
 			dis.close();
-			Thread.sleep(2000);
-			dos.close();
 			Thread.sleep(2000); // wait for data to drain
 			LCD.clear();
 			LCD.drawString(closing,0,0);
 			LCD.refresh();
-			Thread.sleep(2000);
 			btc.close();
 			LCD.clear();
 			
